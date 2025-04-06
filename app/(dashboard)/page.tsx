49ee170d4b9d3da700627c5e1ee0e820b1b96630
dlozano55 <dl6278352@gmail.com>
2025-04-06 18:22:23 -0500

    Title

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
             <div className="sm:text-center md:max-w-2xl md:mx-auto lg:col-span-6 lg:text-left">
               <h1 className="text-4xl font-bold text-gray-900 tracking-tight sm:text-5xl md:text-6xl">
                 RemoLabs
-                <span className="block text-orange-500">NANDOOOO Test Vercel</span>
+                <span className="block text-orange-500">Paid Telegram Groups</span>
               </h1>
               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-xl lg:text-lg xl:text-xl">
                 Launch your SaaS product in record time with our powerful,
